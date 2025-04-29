57e7eac38aafa8b84d1f9d22bf0c0cf35d45eb11
TheQueCode <thequecode@gmail.com>
2025-04-29 14:38:58 -0500

    Lesson 1 section 12 math object update

---
+++ ModernJavaScript/MyFiles/01-variables-data-types/12-math-object/js/script.js
@@ -0,0 +1,25 @@
+console.log(Math);
+
+let x;
+
+x = Math.sqrt(9); // 3
+
+x = Math.abs(-5); // 5 (absolute number)
+
+x = Math.round(4.6); // 5
+
+x = Math.ceil(4.2); // 5
+
+x = Math.floor(4.9); // 4
+
+x = Math.pow(2, 3); // 8
+
+x = Math.min(4, 5, 6, 7, 3); // 3
+
+x = Math.max(4, 5, 6, 7, 3); // 7
+
+x = Math.random(); // random decimal between 0 and 1
+
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
