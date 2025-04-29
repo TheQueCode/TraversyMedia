87a2588269bd249a18c7113591d29d4af5a51cb6
TheQueCode <thequecode@gmail.com>
2025-04-29 11:17:54 -0500

    Lesson 1 section 6 - type conversion update

---
+++ ModernJavaScript/MyFiles/01-variables-data-types/06-type-conversion/js/script.js
@@ -0,0 +1,24 @@
+let amount = 1;
+
+// Convert a string to a number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// Convert a number to a string
+// amount = amount.toString();
+// amount = String(amount);
+
+// Convert a string to a decimal
+// amount = parseFloat(amount);
+
+// Convert number to boolean
+// amount = Boolean(amount); // 1 = true, 0 = false
+
+
+
+
+
+
+
+console.log(amount, typeof amount);
