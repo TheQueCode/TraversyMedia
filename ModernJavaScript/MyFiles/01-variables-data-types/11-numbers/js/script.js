1d12ea9dee1e664176b73c48d3b041327cf08598
TheQueCode <thequecode@gmail.com>
2025-04-29 14:28:30 -0500

    Lesson 1 section 11 numbers update

---
+++ ModernJavaScript/MyFiles/01-variables-data-types/11-numbers/js/script.js
@@ -0,0 +1,25 @@
+const num = 5;
+
+const numNew = new Number(5);
+
+let x;
+
+x = num.toString(); 
+
+x = num.toString.length;
+
+x = num.toFixed(2); // 5.00
+
+x = num.toPrecision(2) // 5.0
+
+x = num.toExponential(2); // 5.00e+0
+
+x = num.toLocaleString('en-US'); // 5
+
+x = num.valueOf(); // 5
+
+x = Number.MAX_VALUE;
+
+x = Number.MIN_VALUE;
+
+console.log(x);
