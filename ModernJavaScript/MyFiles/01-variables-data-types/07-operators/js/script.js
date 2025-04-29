01681b30dcad0964f99b06451bc7e19050832246
TheQueCode <thequecode@gmail.com>
2025-04-29 11:30:20 -0500

    Lesson 1 section 7 operators update

--- ModernJavaScript/MyFiles/01-variables-data-types/07-operators/js/script.js
+++ ModernJavaScript/MyFiles/01-variables-data-types/07-operators/js/script.js
@@ -1,7 +1,54 @@
 // 1. Arithmetic Operators
 
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// Concatenation
+
+x = "Hello" + " " + "World"
+
+// Exponent
+
+x = 2 ** 3 // 8
+
+// Increment 
+
+x = 1;
+// x = x + 1;
+x++;
+
+// Decrement
+
+// x = x - 1;
+x--;
 
 // 2. Assignment Operators
 
+x = 10;
+
+x += 5; // 15
+x -= 5; // 10
+x *= 5; // 50
+x /= 5; // 10
+x %= 5; // 0
+x **= 5;
 
 // 3. Comparison Operators
+
+x = 2 == 2; // true
+x = 2 === 2; // true (tests type)
+
+x = 2 != 2; // false
+x = 2 !== "2"; // true
+
+x = 10 > 5; // true
+x = 10 < 5; // false
+x = 10 <= 5; // false
+x = 10 >= 5; // true
+
+console.log(x);
