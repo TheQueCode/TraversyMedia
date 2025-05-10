5cfa1ecfa96d1380677c7bfc049a08cc2a7c2e03
TheQueCode <thequecode@gmail.com>
2025-05-09 20:12:39 -0500

    section 5 lesson 1 - for loop

--- ModernJavaScript/MyFiles/05-iteration-array-methods/01-for-loop/js/script.js
+++ ModernJavaScript/MyFiles/05-iteration-array-methods/01-for-loop/js/script.js
@@ -5,3 +5,7 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+for (let i = 0; i <= 10; i++){
+  console.log(`Number ${i}`);
+}
