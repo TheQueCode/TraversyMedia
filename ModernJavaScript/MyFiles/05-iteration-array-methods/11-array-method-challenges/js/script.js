b5ec76e800e083db1c2c2498d162ea8b37d871a4
TheQueCode <thequecode@gmail.com>
2025-05-11 17:41:13 -0500

    Section 5 - lesson 11 - Array Methods Challenges - update

--- ModernJavaScript/MyFiles/05-iteration-array-methods/11-array-method-challenges/js/script.js
+++ ModernJavaScript/MyFiles/05-iteration-array-methods/11-array-method-challenges/js/script.js
@@ -37,5 +37,32 @@ const people = [
   },
 ];
 
+const youngPeople = people
+  .filter(ppl => ppl.age <= 25)
+  .map(youngPpl => ({
+    name: youngPpl.firstName + ' ' + youngPpl.lastName,
+    email: youngPpl.email
+  }));
+
+console.log(youngPeople);
+
+
+
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveNums = numbers
+  .filter(number => number > 0)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(positiveNums);
+
+// Challenge 3
+
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words
+  .map(word => word[0].toUpperCase() + word.slice(1))
+
+console.log(capitalizedWords);
+
