e3c1ae31b78066fa6c4bc2d09466770f7abf055d
TheQueCode <thequecode@gmail.com>
2025-05-11 20:30:32 -0500

    Section 6 - DOM intro - update

---
+++ ModernJavaScript/MyFiles/06-document-object-model/01-dom-intro/js/script.js
@@ -0,0 +1,16 @@
+// window
+
+// console.dir(document);
+
+// console.log(document.links[0].innerText);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+// document.write('Hello from JS');
+
+//console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+// main.innerHTML = '<h1>Hello from main</h1>';
+
+document.querySelector('#main h1').innerText = 'Hello';
