db26d23535c229026c1faf847dc8395df8eed7b9
TheQueCode <thequecode@gmail.com>
2025-05-13 17:16:57 -0500

    Section 6 - Lesson 8 - innerHTML vs createElement

---
+++ ModernJavaScript/MyFiles/06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/js/script.js
@@ -0,0 +1,40 @@
+// Quick & Dirty
+
+function createListItem (item)
+{
+  const li = document. createElement('li');
+
+  li.innerHTML = `${item}
+          <button class="remove-item btn-link text-red" title="button">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`
+
+  document.querySelector('.items').appendChild(li);
+}
+
+// Clean & Performant
+
+function createNewItem (item)
+{
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement('button');
+  button.className = 'remove-item btn-link text-red';
+  button.title = 'button';
+
+  const icon = document.createElement('i');
+  icon.className = 'fa-solid fa-xmark';
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+
+}
+
+
+
+
+createListItem('Eggs');
+createNewItem('Cheese');
