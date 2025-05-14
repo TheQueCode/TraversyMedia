780aa4ebdc93170b4e21806dadf32d4a41833119
TheQueCode <thequecode@gmail.com>
2025-05-14 07:25:31 -0500

    Section 6 - Lesson 12 - replacing elements - update

---
+++ ModernJavaScript/MyFiles/06-document-object-model/12-replacing-elements/shopping-list/js/script.js
@@ -0,0 +1,49 @@
+function replaceFirstItem ()
+{
+  const firstItem = document.querySelector('li:first-child');
+
+  const li = document.createElement('li');
+  li.textContent = 'Replaced First Item';
+
+  firstItem.replaceWith(li);
+}
+
+function replaceSecondItem ()
+{
+  const secondItem = document.querySelector('li:nth-child(2)');
+
+  secondItem.outerHTML = '<li>Replaced Second Item</li>';
+}
+
+function replaceAllItems ()
+{
+  const lis = document.querySelectorAll('li');
+/*
+  lis.forEach((item, index) =>
+  {
+    //item.outerHTML = '<li>Replace All</li>';
+    if (index === 1) item.innerHTML = 'Second Item';
+    else item.innerHTML = 'Replace All';
+  });
+*/
+  
+  // Shorter version
+  lis.forEach((item, index) => index === 1 ? item.innerHTML = 'Second Item' : item.innerHTML = 'Replace All');
+}
+
+function replaceChildHeading ()
+{
+  const header = document.querySelector('header');
+  const h1 = document.querySelector('header h1');
+
+  const h2 = document.createElement('h2');
+  h2.id = 'app-title';
+  h2.textContent = 'Shopping List';
+  header.replaceChild(h2, h1);
+}
+
+
+//replaceFirstItem();
+//replaceSecondItem();
+replaceAllItems();
+replaceChildHeading();
