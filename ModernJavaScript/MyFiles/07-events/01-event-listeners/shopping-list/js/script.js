b097089461e2084bca59a4ac53670cb6c6962023
TheQueCode <thequecode@gmail.com>
2025-05-15 08:34:01 -0500

    Section 7 - Lesson 1 - Event listeners pausing

---
+++ ModernJavaScript/MyFiles/07-events/01-event-listeners/shopping-list/js/script.js
@@ -0,0 +1,24 @@
+function onClear ()
+{
+  console.log('On Clear');
+}
+const clearBtn = document.querySelector('#clear');
+
+// JavaScript Event Listener
+/*
+clearBtn.onclick = function ()
+{
+  console.log('Clear Items');
+}
+*/
+
+// Add event Listener
+// clearBtn.addEventListener('click', () => console.log('Clear Items'));
+//clearBtn.addEventListener('click', onClear);
+
+// when removing, remove event listener first
+
+//setTimeout(() => clearBtn.removeEventListener('click', onClear),5000); 
+
+//setTimeout(() => clearBtn.click(), 5000);
+
