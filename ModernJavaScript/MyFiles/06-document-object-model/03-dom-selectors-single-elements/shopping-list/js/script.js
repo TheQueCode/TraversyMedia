c355727982c256ba5b444549228174a08b5e4010
TheQueCode <thequecode@gmail.com>
2025-05-17 11:38:23 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/03-dom-selectors-single-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/03-dom-selectors-single-elements/shopping-list/js/script.js
@@ -1,23 +1,65 @@
 // Revisiting Section 6
 
+// Selecting single elements
+
+// document.getElementById();
+
+console.log(document.getElementById('app-title')); // <h1 id="app-title">Shopping List</h1>
+console.log(document.getElementById('app-title').id); //app-title
+console.log(document.getElementById('app-title').className); //boo
+
+console.log(document.getElementById('app-title').getAttribute('id')); //app-title
+console.log(document.getElementById('app-title').getAttribute('class')); //boo
+
+// Set attributes
+document.getElementById('app-title').title = 'Shopping List';
+
+document.getElementById('app-title').setAttribute('class', 'title');
+console.log(document.getElementById('app-title').className); //title
+
+const title = document.getElementById('app-title');
+
+console.log(title); // <h1 id="app-title" class="title" title="Shopping List">Shopping List</h1>
+
+// Get or change element content
+
+console.log(title.textContent); // Shopping List
+title.textContent = 'Hello World';
+title.innerText = 'Hello World';
+title.innerHTML = '<strong>Shopping List</strong>';
+
+// Change styles
+title.style.color = 'red';
+title.style.backgroundColor = 'black';
+title.style.padding = '10px';
+title.style.borderRadius = '10px';
 
 
+// document.querySelector()
 
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#app-title'));
+console.log(document.querySelector('.container'));
+console.log(document.querySelector('input[type="text"]'));
+console.log(document.querySelector('li:nth-child(2)').innerText); // Orange Juice
 
+const secondItem = document.querySelector('li:nth-child(2)');
+secondItem.innerText = 'Apple Juice';
+secondItem.style.color = 'Orange';
 
+// Use these methods on other elements
 
+const list = document.querySelector('ul');
+console.log(list.innerText); 
+// Apples
+// Apple Juice
+// Oreos
+// Milk
 
+const firstItem = list.querySelector('li:first-child');
+console.log(firstItem.innerText); // Apples
 
-
-
-
-
-
-
-
-
-
-
+firstItem.style.color = 'green';
 
 
 
