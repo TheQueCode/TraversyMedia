e790021f4873add3482b3080353729fb0c80e7fb
TheQueCode <thequecode@gmail.com>
2025-05-17 18:39:29 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/07-create-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/07-create-elements/shopping-list/js/script.js
@@ -1,23 +1,21 @@
 // Revisiting Section 6
 
+// create a new div
 
+const div = document.createElement('div');
+div.className = 'my-element';
+div.id = 'my-element';
+div.setAttribute('title', 'my-element');
 
+// create text node & then append child
 
+const text = document.createTextNode('This is my element. There are many like it, but this one is mine.');
+div.appendChild(text);
 
+//document.body.appendChild(div);
+document.querySelector('ul').appendChild(div);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//console.log(div); // <div></div>
 
 
 /*
