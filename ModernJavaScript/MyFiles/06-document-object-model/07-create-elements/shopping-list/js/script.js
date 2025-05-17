a97cdd8ec760b3ad092e266975d4d1968afdc155
TheQueCode <thequecode@gmail.com>
2025-05-17 18:25:03 -0500

    Revisiting Section 6 - HTML tags & JS notes

--- ModernJavaScript/MyFiles/06-document-object-model/07-create-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/07-create-elements/shopping-list/js/script.js
@@ -1,3 +1,26 @@
+// Revisiting Section 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const div = document.createElement('div');
 div.className = 'my-element';
 div.id = 'my-element';
@@ -9,8 +32,4 @@ div.appendChild(text);
 // document.body.appendChild(div);
 
 document.querySelector('ul').appendChild(div);
-
-
-
-
-
+*/
