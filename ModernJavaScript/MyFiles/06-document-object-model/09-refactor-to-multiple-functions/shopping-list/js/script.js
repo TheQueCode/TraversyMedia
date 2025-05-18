5120b36dcac718b27ebdfd87323039d6fb258b0e
TheQueCode <thequecode@gmail.com>
2025-05-17 21:12:40 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/js/script.js
@@ -1,8 +1,40 @@
 // Revisiting Section 6
 
+function createNewItem (item)
+{
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
 
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
 
+  document.querySelector('.items').appendChild(li);
+}
 
+function createButton (classes)
+{
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.title = 'Remove';
+  button.className = classes;
+
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon (classes)
+{
+  const icon = document.createElement('i');
+  icon.className = classes;
+  
+  return icon;
+}
+
+createNewItem('Pizza');
+createNewItem('Steak');
+createNewItem('Spaghetti')
 
 
 
