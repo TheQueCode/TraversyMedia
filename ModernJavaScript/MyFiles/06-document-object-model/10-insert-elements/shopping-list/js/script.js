cfeb16594f57a74dab476485dcdf0e935ae4dd1e
TheQueCode <thequecode@gmail.com>
2025-05-18 08:19:44 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/10-insert-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/10-insert-elements/shopping-list/js/script.js
@@ -1,6 +1,63 @@
 // Revisiting Section 6
 
+// InsertAdjacentElement - to insert a new custom element
 
+function insertElement ()
+{
+  const filter = document.querySelector('.filter');
+
+  const newh1 = document.createElement('h1');
+  newh1.textContent = 'insertAdjacentElement';
+
+  filter.insertAdjacentElement('beforebegin', newh1 );
+}
+
+// InsertAdjacentText - to insert text
+
+function insertText ()
+{
+  const item = document.querySelector('li:first-child');
+
+  item.insertAdjacentText('afterend', 'insertAdjacentText');
+}
+
+// InsertAdjacentHTML - to insert HTML
+
+function insertHTML ()
+{
+  const clearBtn = document.querySelector('#clear');
+
+  clearBtn.insertAdjacentHTML('beforebegin', '<h2>insertAdjacentHTML</h2>');
+}
+
+// insertBefore - similar to appendChild
+
+function insertBeforeItem ()
+{
+  const ul = document.querySelector('ul');
+
+  const li = document.createElement('li');
+  li.textContent = 'insertBefore';
+
+  const thirdItem = document.querySelector('li:nth-child(3)');
+
+  ul.insertBefore(li, thirdItem);
+}
+
+insertElement();
+insertText();
+insertHTML();
+insertBeforeItem();
+
+/*
+<!-- beforebegin -->
+<p>
+  <!-- afterbegin -->
+  blarty blarty blar
+  <!-- beforeend -->
+</p>
+<!-- afterend -->
+*/
 
 
 
