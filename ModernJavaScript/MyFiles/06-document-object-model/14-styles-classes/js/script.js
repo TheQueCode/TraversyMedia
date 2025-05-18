27fd84cbc21622bd02cd039a67181d353f7a9aa4
TheQueCode <thequecode@gmail.com>
2025-05-18 17:52:32 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/14-styles-classes/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/14-styles-classes/js/script.js
@@ -1,3 +1,76 @@
+// Revisiting Section 6
+
+const paragraph = document.querySelector('p');
+const itemList = document.querySelector('.item-list');
+const items = itemList.querySelectorAll('li');
+
+function run ()
+{
+  //console.log(itemList.className);
+  //paragraph.className = 'card dark';
+
+  // classList
+  //console.log(itemList.classList);
+
+  itemList.classList.forEach(c => console.log(c));
+
+  //paragraph.classList.add('dark');
+  //paragraph.classList.remove('card');
+
+  // Toggle
+
+  //paragraph.classList.toggle('dark');
+  //paragraph.classList.toggle('hidden');
+  //paragraph.classList.replace('card', 'dark');
+
+  // Change style
+  //itemList.style.lineHeight = '3';
+
+  items.forEach((item, index) => index === 2 ? item.style.color = 'blue' : item.style.color = 'red');
+}
+
+
+document.querySelector('button').onclick = run;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const text = document.querySelector('p');
 const itemList = document.querySelector('.item-list');
 const items = document.querySelectorAll('li');
@@ -30,3 +103,4 @@ function run ()
   });
 }
 document.querySelector('button').onclick = run;
+*/
