37d154b36565960e63558afee0dcb370d9e70bf8
TheQueCode <thequecode@gmail.com>
2025-05-17 19:44:03 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/js/script.js
@@ -1,42 +1,61 @@
 // Revisiting Section 6
 
+// Create Element & Add to the DOM
 
 
+// Try 1
 
 
+function addOn (item)
+{
+  const parent = document.querySelector('ul');
+
+  const newLi = document.createElement('li');
+  newLi.innerHTML = `${item} <button class="remove-item btn-link text-red" title="Remove" type="button">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+  
+  parent.appendChild(newLi);
+};
+
+addOn('Eggs');
+addOn('Bacon');
+addOn('Cheese');
+addOn('Pizza');
 
 
+// Try 2
 
 
+function anotherAddOn (item)
+{
+  const parent = document.querySelector('ul');
+  
+  const newLi = document.createElement('li');
 
+  const newText = document.createTextNode(item);
+  
+  const newBtn = document.createElement('button');
+  newBtn.className = 'remove-item btn-link text-red';
+  newBtn.title = 'Remove';
+  newBtn.type = 'button';
+  newLi.appendChild(newBtn);
+  
+  const newI = document.createElement('i');
+  newI.className = 'fa-solid fa-xmark';
 
+  parent.appendChild(newLi)
+  newLi.appendChild(newText)
+  newLi.appendChild(newBtn);
+  newBtn.appendChild(newI);
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+anotherAddOn('Grapes');
+anotherAddOn('Tomatoes');
+anotherAddOn('Salad');
+anotherAddOn('Potatos');
+anotherAddOn('Steak');
 
 
 
