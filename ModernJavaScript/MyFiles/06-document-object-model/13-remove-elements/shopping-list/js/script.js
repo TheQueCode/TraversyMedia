fcab046ad418cb31ca5d6b32f84e7990ce766d59
TheQueCode <thequecode@gmail.com>
2025-05-18 17:19:45 -0500

    Revisiting Section 6

--- ModernJavaScript/MyFiles/06-document-object-model/13-remove-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/13-remove-elements/shopping-list/js/script.js
@@ -1,4 +1,62 @@
-function removeClearButton(){
+// Revisiting Section 6
+
+// Removing DOM elements
+
+// Remove & Remove Child - [Remove: on element you want removed] - [Remove child called on parent of element you want to remove]
+/*
+function removeClearBtn ()
+{
+  document.querySelector('#clear').remove();
+}
+
+removeClearBtn();
+
+function removeFirstItem ()
+{
+  const parent = document.querySelector('ul');
+  const li = document.querySelector('li:first-child');
+
+  parent.removeChild(li);
+}
+
+removeFirstItem();
+
+function removeItem (itemNumber)
+{
+  document.querySelector(`li:nth-child(${itemNumber})`).remove();
+}
+
+removeItem(1);
+
+
+function removeItem2 (itemNumber)
+{
+  const ul = document.querySelector('ul');
+  const li = document.querySelectorAll('li')[itemNumber - 1]; 
+
+  ul.removeChild(li);
+}
+
+removeItem2(1);
+
+
+function removeItem3 (itemNummber)
+{
+  const li = document.querySelectorAll('li');
+  li[itemNummber - 1].remove();
+}
+
+removeItem3(1);
+
+const removeItem4 = itemNumber => document.querySelectorAll('li')[itemNumber - 1].remove();
+
+
+removeItem4();
+*/
+
+/*
+function removeClearButton ()
+{
   const clearBtn = document.querySelector('#clear')
   clearBtn.remove();
 }
@@ -41,3 +99,4 @@ const removeItem4 = itemNumber => document.querySelectorAll('li')[itemNumber -1]
 //removeItem2(1);
 //removeItem3(3);
 //removeItem4(2);
+*/
