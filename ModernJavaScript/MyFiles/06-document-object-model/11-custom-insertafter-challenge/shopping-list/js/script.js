b6236845c5923023f2228bc85cf64afe31b1e563
TheQueCode <thequecode@gmail.com>
2025-05-18 08:54:50 -0500

    Revisiting Section 6 - HTML tags & JS Notes

--- ModernJavaScript/MyFiles/06-document-object-model/11-custom-insertafter-challenge/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/11-custom-insertafter-challenge/shopping-list/js/script.js
@@ -1,9 +1,34 @@
 // Revisiting Section 6
 
+// Making a custom insertAfter()
+
+function insertAfter (newEl, existingEl)
+{
+  /*
+  const next = existingEl.nextElementSibling;
+
+  const parent = existingEl.parentElement;
+  parent.insertBefore(newEl, next);
+  */
+  existingEl.parentElement.insertBefore(newEl, existingEl.nextElementSibling);
+}
+
+const li = document.createElement('li');
+li.textContent = 'Insert Me After!';
+
+const firstItem = document.querySelector('li:first-child');
 
 
+insertAfter(li, firstItem);
 
 
+const boo = document.createElement('li');
+boo.textContent = "Holy Poo! I gotta pee."
+
+const notFirstItem = document.querySelector('li:nth-child(4)');
+
+
+insertAfter(boo, notFirstItem);
 
 
 
