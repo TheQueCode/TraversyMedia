f6c88639f553c1c94021f0a843ccd3fc9e467a4e
TheQueCode <thequecode@gmail.com>
2025-05-18 12:05:59 -0500

    Revisiting Seciton 6

--- ModernJavaScript/MyFiles/06-document-object-model/12-replacing-elements/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/06-document-object-model/12-replacing-elements/shopping-list/js/script.js
@@ -1,14 +1,56 @@
 // Revisiting Section 6
 
+// Replacing Elements
+
+function replaceFirstItem ()
+{
+  const firstItem = document.querySelector('li:first-child');
+
+  const li = document.createElement('li');
+  li.textContent = 'Replaced First';
+
+  firstItem.replaceWith(li);
+}
 
 
+replaceFirstItem();
 
 
+function replaceSecondItem ()
+{
+  const secondItem = document.querySelector('li:nth-child(2)');
+
+  secondItem.outerHTML = '<li>Replaced Second</li>';
+}
 
 
+replaceSecondItem();
 
 
+function replaceAll ()
+{
+  const items = document.querySelectorAll('li');
 
+  items.forEach((item, index) => index === 1 ? item.outerHTML = '<li>Second Item</li>' : item.textContent = 'Replaced with forEach');
+}
+
+replaceAll();
+
+
+function replaceChildHeading ()
+{
+  const header = document.querySelector('header');
+  const h1 = document.querySelector('h1');
+  
+  const h2 = document.createElement('h2');
+  h2.id = 'app-title';
+  h2.textContent = 'Shopping List';
+
+  header.replaceChild(h2, h1);
+
+}
+
+replaceChildHeading();
 
 
 
