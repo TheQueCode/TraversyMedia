ab5788d219888f746e56282dabffdfa5a4af1bba
TheQueCode <thequecode@gmail.com>
2025-05-20 07:44:50 -0500

    Section 7 - Lesson 3 - event objects - update

--- ModernJavaScript/MyFiles/07-events/03-event-object/shopping-list/js/script.js
+++ ModernJavaScript/MyFiles/07-events/03-event-object/shopping-list/js/script.js
@@ -1,3 +1,44 @@
+const logo = document.querySelector('img');
+
+function onDrag (e)
+{
+  document.querySelector('h1').textContent = `X: ${e.clientX} Y: ${e.clientY}`;
+}
+logo.addEventListener('click', e => console.log(e));
+logo.addEventListener('click', e => console.log(e.target));
+logo.addEventListener('click', e => console.log(e.currentTarget));
+// Inside a function this would work (e.target.style.backgroundColor = 'black';)
+logo.addEventListener('click', e => console.log(e.type)); //click
+logo.addEventListener('click', e => console.log(e.timeStamp)); 
+logo.addEventListener('click', e => console.log(e.clientX)); // horizontal
+logo.addEventListener('click', e => console.log(e.clientY)); //vertical
+logo.addEventListener('click', e => console.log(e.offsetX)); 
+logo.addEventListener('click', e => console.log(e.offsetY)); 
+logo.addEventListener('click', e => console.log(e.pageX)); 
+logo.addEventListener('click', e => console.log(e.pageY)); 
+logo.addEventListener('click', e => console.log(e.screenX)); 
+logo.addEventListener('click', e => console.log(e.screenY)); 
+
+logo.addEventListener('drag', onDrag);
+
+// prevent default behavior
+
+document.querySelector('a').addEventListener('click', e =>
+{
+  e.preventDefault()
+  console.log('Link was clicked')
+});
+
+
+
+
+/*
+document.body.addEventListener('click', function (e)
+{
+  console.log(e.target);
+  console.log(e.currentTarget);
+});
+*/
 /*
 - `target` - The element that triggered the event
 - `currentTarget` - The element that the event listener is attached to (These are the same in this case
